9985ce03efa6f50e7e142e6d2a05495dda346ff0
bburwel1 <benjamin.burwell@optum.com>
2021-05-14 13:04:47 -0500

    chapter 1 and 2

---
+++ Chapter 2/hello_world2.py
@@ -0,0 +1,10 @@
+
+message = "Hello 2"
+print(message)
+
+#try it yourself 
+simpleMessage = "Hey"
+print(simpleMessage)
+
+simplemessage2 = 'hello'
+print(simplemessage2)
---
+++ Chapter 2/name.py
@@ -0,0 +1,31 @@
+#chapter 2 of Python Crash Course
+
+# name = 'hello my name is ben'
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+
+# first_name = "ben"
+# last_name = "burwell"
+# full_name = f'{first_name} {last_name}'
+# print(f'Hello, {full_name.title()}')
+
+# print("Dogs:\n\tLilly\n\tTwiggy")
+
+# favorite_language = 'python   '
+# print(favorite_language.lstrip() + 'hey')
+
+# #try yourself
+# persons_name = 'ben'
+# print(f'hey {persons_name}')
+# print(persons_name.lower())
+# print(persons_name.upper())
+# print(persons_name.title())
+
+# print('Ben once said "Hey we are testing quotes"')
+
+# name_space = "  ben   "
+# print(name_space.strip())
+
+# favnum = 3 
+# print(favnum)
